3b2309f5e52ee8d11eb22feecb91510f90dff896
Ariel Yumul <ayumul@eqc.govt.nz>
2024-06-14 13:53:38 +1200

    update on provider

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "~> 3.0.2"
+      version = "~> 3.98.2"
     }
   }
   cloud {
@@ -16,4 +16,5 @@ terraform {
 
 provider "azurerm" {
   features {}
+  skip_provider_registration = true
 }
