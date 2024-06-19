7800e5ffc5312a133c2081bfe40949c1bed920a0
Ariel Yumul <ayumul@eqc.govt.nz>
2024-06-20 10:09:26 +1200

    remove 2 sa

--- storageaccount.tf
+++ storageaccount.tf
@@ -17,30 +17,30 @@ resource "azurerm_storage_account" "StorageAccountDemo" {
   }
 }
 
-## Another test
-resource "azurerm_storage_account" "StorageAccountDemo2" {
-  name                     = "mysagitadotfc01"
-  resource_group_name      = azurerm_resource_group.demo.name
-  location                 = azurerm_resource_group.demo.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
+# ## Another test
+# resource "azurerm_storage_account" "StorageAccountDemo2" {
+#   name                     = "mysagitadotfc01"
+#   resource_group_name      = azurerm_resource_group.demo.name
+#   location                 = azurerm_resource_group.demo.location
+#   account_tier             = "Standard"
+#   account_replication_type = "GRS"
 
-  tags = {
-    video   = "azure"
-    channel = "CloudQuickLabs"
-  }
-}
+#   tags = {
+#     video   = "azure"
+#     channel = "CloudQuickLabs"
+#   }
+# }
 
-## Another test
-resource "azurerm_storage_account" "StorageAccountDemo3" {
-  name                     = "mysahubtfc01"
-  resource_group_name      = azurerm_resource_group.demo.name
-  location                 = azurerm_resource_group.demo.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
+# ## Another test
+# resource "azurerm_storage_account" "StorageAccountDemo3" {
+#   name                     = "mysahubtfc01"
+#   resource_group_name      = azurerm_resource_group.demo.name
+#   location                 = azurerm_resource_group.demo.location
+#   account_tier             = "Standard"
+#   account_replication_type = "GRS"
 
-  tags = {
-    video   = "azure"
-    channel = "CloudQuickLabs"
-  }
-}
+#   tags = {
+#     video   = "azure"
+#     channel = "CloudQuickLabs"
+#   }
+# }
