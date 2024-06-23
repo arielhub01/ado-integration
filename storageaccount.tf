f88963971bc1767c6d2af3ebef13d261ea86ba50
Ariel Yumul <ayumul@eqc.govt.nz>
2024-06-24 09:42:49 +1200

    update on sa naming

--- storageaccount.tf
+++ storageaccount.tf
@@ -19,7 +19,7 @@ resource "azurerm_resource_group" "demo" {
 
 # ## Another test
 resource "azurerm_storage_account" "StorageAccountDemo2" {
-  name                     = "mysaworkspace01325"
+  name                     = "sa${var.work_env}ws01325"
   resource_group_name      = azurerm_resource_group.demo.name
   location                 = azurerm_resource_group.demo.location
   account_tier             = "Standard"
