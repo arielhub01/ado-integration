e5f6ed5596b44532d4115f8a45517c0104a37119
Ariel Yumul <ayumul@eqc.govt.nz>
2024-06-24 11:37:27 +1200

    skip checkov checks

--- storageaccount.tf
+++ storageaccount.tf
@@ -5,6 +5,16 @@ resource "azurerm_resource_group" "demo" {
 
 # ## Another test
 resource "azurerm_storage_account" "StorageAccountDemo2" {
+  #checkov:skip=CKV_AZURE_33: "Ensure Storage logging is enabled for Queue service for read, write and delete requests"
+  #checkov:skip=CKV_AZURE_190: "Ensure that Storage blobs restrict public access"
+  #checkov:skip=CKV_AZURE_44: "Ensure Storage Account is using the latest version of TLS encryption"
+  #checkov:skip=CKV_AZURE_59: "Ensure that Storage accounts disallow public access"
+  #checkov:skip=CKV_AZURE_40: "Ensure storage account is not configured with Shared Key authorization"
+  #checkov:skip=CKV_AZURE_38: "Ensure soft-delete is enabled on Azure storage account"
+  #checkov:skip=CKV2_AZURE_33: "Ensure storage account is configured with private endpoint"
+  #checkov:skip=CKV2_AZURE_41: "Ensure storage account is configured with SAS expiration policy"
+  #checkov:skip=CKV2_AZURE_47: "Ensure storage account is configured without blob anonymous access"
+  #checkov:skip=CKV2_AZURE_1: "Ensure storage for critical data are encrypted with Customer Managed Key"
   name                     = "sa${var.work_env}ws01325"
   resource_group_name      = azurerm_resource_group.demo.name
   location                 = azurerm_resource_group.demo.location
